9f253be9cf9bda8ffc4f352dfe9cd55bdee0107b
ivipllp <1343626326@qq.com>
2017-01-17 01:44:25 +0800

    增加:主页布局

--- src/pages/electricity/electricity.js
+++ src/pages/electricity/electricity.js
@@ -6,6 +6,17 @@ Page({
   },
   onLoad () {
     // TODO: onLoad
+    // wx.request({
+    //   url: "https://hongyan.cqupt.edu.cn/MagicLoop/index.php?s=/addon/ElectricityQuery/ElectricityQuery/queryElecByRoom",
+    //   method: "POST",
+    //   data: {
+    //     building: 15,
+    //     room: 232
+    //   },
+    //   success: function (res) {
+    //     console.log(res);
+    //   }
+    // })
   },
   onReady () {
     // TODO: onReady
