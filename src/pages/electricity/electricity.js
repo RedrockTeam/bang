33739388dc9e9efa43dd97e83edcc2e70bebde5b
ivipllp <1343626326@qq.com>
2017-01-17 16:27:41 +0800

    电费主页，还没有填数据

--- src/pages/electricity/electricity.js
+++ src/pages/electricity/electricity.js
@@ -2,21 +2,38 @@
 
 Page({
   data: {
-    title: 'electricity'
+    title: 'electricity',
+    elecState: {}
+  },
+  changeDormInfor: function (e) {
+
   },
   onLoad () {
     // TODO: onLoad
-    // wx.request({
-    //   url: "https://hongyan.cqupt.edu.cn/MagicLoop/index.php?s=/addon/ElectricityQuery/ElectricityQuery/queryElecByRoom",
-    //   method: "POST",
-    //   data: {
-    //     building: 15,
-    //     room: 232
-    //   },
-    //   success: function (res) {
-    //     console.log(res);
-    //   }
-    // })
+    wx.request({
+      url: `http://hongyan.cqupt.edu.cn/MagicLoop/index.php?s=/addon/ElectricityQuery/ElectricityQuery/getElectric`,
+      method: `POST`,
+      header: {
+        'Content-Type': `application/x-www-form-urlencoded`
+      },
+      data: {
+        stuNum: 2015211878
+      },
+      success: (res) => {
+        if (res.statusCode === 200) {
+          if (res.data.status === 200) {
+            console.log(res);
+            this.setData({
+              elecState: res.data.data
+            });
+          } else {
+            console.log('网络错误!');
+          }
+        } else {
+          console.log('网络错误!');
+        }
+      }
+    });
   },
   onReady () {
     // TODO: onReady
