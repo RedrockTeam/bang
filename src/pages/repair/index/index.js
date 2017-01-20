188a2dd1aeb8f74ad5367e77926405b33bd4dfa7
Hangeer <591417089@qq.com>
2017-01-21 00:04:07 +0800

    update:更新页面，开始对接口写逻辑

--- src/pages/repair/index/index.js
+++ src/pages/repair/index/index.js
@@ -2,6 +2,48 @@
 
 Page({
   data: {
-    title: '报修服务'
+    userInfo: {
+      stuNum: 2015211535
+    },
+    repairList: {
+      finish: [],
+      send: [],
+      verify: []
+    },
+    apiPrefix: 'http://hongyan.cqupt.edu.cn/MagicLoop/index.php?s=/addon/RepairAndSuggest/Repair'
+  },
+  onLoad () {
+    let self = this;
+
+    wx.showToast({
+      title: '数据获取中',
+      icon: 'loading',
+      duration: 10000
+    });
+    // 转个菊花
+    wx.request({
+      url: `${self.data.apiPrefix}/getRepairInfo`,
+      method: 'post',
+      data: {
+        stunum: self.data.userInfo.stuNum
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      success: function (res) {
+        let resData = res.data.data;
+        let dataList = {
+          verify: resData.verify,
+          send: resData.send,
+          finish: resData.finish
+        };
+
+        wx.hideToast();
+        self.setData({
+          repairList: dataList
+        });
+      }
+    });
   }
+  // 页面渲染之前发个请求
 });
