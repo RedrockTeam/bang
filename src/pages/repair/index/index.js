c2ad542e98bb35058bc413c090281ed1d156ee4e
Hangeer <591417089@qq.com>
2017-01-21 17:39:04 +0800

    update: 解决了一些样式问题

--- src/pages/repair/index/index.js
+++ src/pages/repair/index/index.js
@@ -3,14 +3,16 @@
 Page({
   data: {
     userInfo: {
-      stuNum: 2015211535
+      // stuNum: 2015211535
+      stuNum: 2014211767
     },
     repairList: {
       finish: [],
       send: [],
       verify: []
     },
-    apiPrefix: 'http://hongyan.cqupt.edu.cn/MagicLoop/index.php?s=/addon/RepairAndSuggest/Repair'
+    apiPrefix: 'http://hongyan.cqupt.edu.cn/MagicLoop/index.php?s=/addon/RepairAndSuggest/Repair',
+    emptyData: true
   },
   onLoad () {
     let self = this;
@@ -21,6 +23,7 @@ Page({
       duration: 10000
     });
     // 转个菊花
+
     wx.request({
       url: `${self.data.apiPrefix}/getRepairInfo`,
       method: 'post',
@@ -32,18 +35,33 @@ Page({
       },
       success: function (res) {
         let resData = res.data.data;
-        let dataList = {
-          verify: resData.verify,
-          send: resData.send,
-          finish: resData.finish
-        };
+        let dataList = {};
+        let dataEmpty = true;
 
+        if (~~resData.count > 0) {
+          dataList = {
+            verify: resData.verify,
+            send: resData.send,
+            finish: resData.finish
+          };
+          dataEmpty = false;
+          for (let key in dataList) {
+            dataList[key].forEach((item) => {
+              if (item.wx_bxsj) {
+                item.wx_bxsj = item.wx_bxsj.split(' ')[0].split('-').join('/');
+              }
+            });
+          }
+        }
+        // 针对返回数据格式做一些调整
         wx.hideToast();
+        // 菊花转完
         self.setData({
-          repairList: dataList
+          repairList: dataList,
+          emptyData: dataEmpty
         });
+        // 更新数据
       }
     });
   }
-  // 页面渲染之前发个请求
 });
