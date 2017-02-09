35b952575e87f48206c0a9b754e522f54b5caf4a
shuishui123 <572239330@qq.com>
2017-02-09 21:40:20 +0800

    style: 修改代码风格以通过 eslint

--- src/pages/date/class/class.js
+++ src/pages/date/class/class.js
@@ -1,10 +1,10 @@
 Page({
   data: {
-    imgUrl: "../../../images",
+    imgUrl: '../../../images',
     hiddenFlag: true,
-    array: ['本学期','第一周', '第二周', '第三周', '第四周', '第五周', '第六周', '第七周', '第八周', '第九周', '第十周', '第十一周', '第十二周', '第十三周', '第十四周', '第十五周', '第十六周', '第十七周', '第十八周', '第十九周', '第二十周'],
+    array: ['本学期', '第一周', '第二周', '第三周', '第四周', '第五周', '第六周', '第七周', '第八周', '第九周', '第十周', '第十一周', '第十二周', '第十三周', '第十四周', '第十五周', '第十六周', '第十七周', '第十八周', '第十九周', '第二十周'],
     index: 0,
-    name: "曾玉",
+    name: '曾玉',
     stuNumber: '2014210104',
     color: [[], [], [], [], [], []],
     className: {
@@ -23,48 +23,46 @@ Page({
       nineT: [],
       elevenT: []
     },
-    classWeekday: ['周一','周二','周三','周四','周五','周六','周日'],
-    detailWeek: "",
-    detailTime: "",
-    detailClassname: "",
-    detailClassroom: ""
+    classWeekday: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+    detailWeek: '',
+    detailTime: '',
+    detailClassname: '',
+    detailClassroom: ''
   },
   // 切换周数
-  bindPickerChange: function bindPickerChange(e) {
+  bindPickerChange: function bindPickerChange (e) {
     this.setData({
       index: e.detail.value
     });
-    
     console.log(this.data.index);
     this.dataRequest();
     // this.onLoad();
   },
   // setInterval(onLoad,3000);
-  onLoad: function onLoad(params) {
-    
+  onLoad: function onLoad (params) {
     // console.log(params);
     // 确定当前周数
-    var self = this;
+    let self = this;
     self.setData({
       stuNumber: params.stuNumber,
-      name: params.stuName,
+      name: params.stuName
     });
     wx.request({
       url: 'http://hongyan.cqupt.edu.cn/redapi2/api/kebiao',
       method: 'post',
       data: {
         stuNum: self.data.stuNumber,
-        idNum: "",
-        week: ""
+        idNum: '',
+        week: ''
       },
       header: {
         'content-type': 'application/x-www-form-urlencoded'
       },
-      success: function(res) {
+      success: function (res) {
         self.setData({
           index: res.data.nowWeek
         });
-        if(self.data.index >20 ) {
+        if (self.data.index > 20) {
           self.setData({
             index: 0
           });
@@ -75,38 +73,36 @@ Page({
     this.dataRequest();
   },
   // 数据请求
-  dataRequest: function dataRequest(e) {
-    var self = this;
-    var dataIndex = parseInt(self.data.index);
+  dataRequest: function dataRequest (e) {
+    let self = this;
+    let dataIndex = parseInt(self.data.index);
     wx.request({
       url: 'http://hongyan.cqupt.edu.cn/redapi2/api/kebiao',
       method: 'post',
       data: {
         stuNum: self.data.stuNumber,
-        idNum: "",
+        idNum: '',
         week: dataIndex
       },
       header: {
         'content-type': 'application/x-www-form-urlencoded'
       },
-      success: function success(res) {
+      success: function success (res) {
         console.log(res.data);
-        var classData = res.data.data;
-        var classLen = classData.length;
-        var classWeek = ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期天'];
-        var classTime = ['一二节', '三四节', '五六节', '七八节', '九十节', '十一十二节'];
-
-        var className = [[], [], [], [], [], []];
-        var classRoom = [[], [], [], [], [], []];
-        var color = [[], [], [], [], [], []];
-
+        let classData = res.data.data;
+        let classLen = classData.length;
+        let classWeek = ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期天'];
+        let classTime = ['一二节', '三四节', '五六节', '七八节', '九十节', '十一十二节'];
+        let className = [[], [], [], [], [], []];
+        let classRoom = [[], [], [], [], [], []];
+        let color = [[], [], [], [], [], []];
         // 课表数据绑定
-        for (var i = 0; i < classLen; i++) {
+        for (let i = 0; i < classLen; i++) {
           // console.log(classData[i].day);
-          for (var t = 0; t < classTime.length; t++) {
-            if (classData[i].lesson == classTime[t]) {
-              for (var j = 0; j < classWeek.length; j++) {
-                if (classData[i].day == classWeek[j]) {
+          for (let t = 0; t < classTime.length; t++) {
+            if (classData[i].lesson === classTime[t]) {
+              for (let j = 0; j < classWeek.length; j++) {
+                if (classData[i].day === classWeek[j]) {
                   className[t][j] = classData[i].course;
                   classRoom[t][j] = classData[i].classroom;
                 }
@@ -115,10 +111,10 @@ Page({
           }
         }
         // 背景颜色绑定
-        for (var a = 0; a < classTime.length; a++) {
-          for (var b = 0; b < classWeek.length; b++) {
-            if (className[a][b] == null) {
-              color[a][b] = "#fff";
+        for (let a = 0; a < classTime.length; a++) {
+          for (let b = 0; b < classWeek.length; b++) {
+            if (!className[a][b]) {
+              color[a][b] = '#fff';
             }
           }
           // console.log(className);
@@ -149,20 +145,19 @@ Page({
     });
   },
   // 弹窗出现
-  bindAppear: function bindAppear(e) {
+  bindAppear: function bindAppear (e) {
     // console.log(e);
-    if(e.currentTarget.dataset.classname) {
+    if (e.currentTarget.dataset.classname) {
       this.setData({
         hiddenFlag: !this.data.hiddenFlag,
         detailWeek: e.currentTarget.dataset.classweek,
         detailTime: e.currentTarget.dataset.classtime,
         detailClassname: e.currentTarget.dataset.classname,
         detailClassroom: e.currentTarget.dataset.classroom
-      });  
+      });
     }
-    
   },
-  bindDisappear:function bindDisappear(e) {
+  bindDisappear: function bindDisappear (e) {
     this.setData({
       hiddenFlag: !this.data.hiddenFlag
     });
--- src/pages/date/index/index.js
+++ src/pages/date/index/index.js
@@ -1,30 +1,28 @@
 
-
 Page({
   data: {
-    imgUrl: "../../../images",
+    imgUrl: '../../../images',
     firstHidden: true,
     deleteHidden: true,
     resultHidden: true,
     nobodyHidden: false,
     somebodyHidden: true,
-    coverHidden: true,  
+    coverHidden: true,
     searchResult: {
       stuName: [],
-      stuNum: [],
+      stuNum: []
     },
-    inputValue: "",
-    resultHight: "",
+    inputValue: '',
+    resultHight: '',
     addStu: {
       stuName: [],
       stuNum: [],
       stuMajor: [],
-      stuClass: [],
+      stuClass: []
     },
-    anotherAdd: 6,
+    anotherAdd: 6
   },
   onLoad (params) {
-    var self = this;
     wx.request({
       url: 'http://hongyan.cqupt.edu.cn/api/stuinfo',
       method: 'get',
@@ -34,29 +32,28 @@ Page({
       header: {
         'content-type': 'application/x-www-form-urlencoded'
       },
-      success: function(res) {
+      success: function (res) {
         console.log(res.data);
       // console.log(self.data.index);
       }
     });
   },
-  bindinputChange: function bindinputChange(e) {
-    var value = e.detail.value;
+  bindinputChange: function bindinputChange (e) {
+    let value = e.detail.value;
     // console.log(value);
     this.setData({
-      inputValue: value,
+      inputValue: value
     });
   },
-  searchTap: function searchTap(e) {
-    var self = this;
-    var stuName = [];
-    var stuNum = [];
+  searchTap: function searchTap (e) {
+    let self = this;
+    let stuName = [];
+    let stuNum = [];
     wx.showToast({
       title: '数据获取中',
       icon: 'loading',
-      duration: 5000
+      duration: 10000
     });
-    
     wx.request({
       url: 'http://hongyan.cqupt.edu.cn/api/stuinfo',
       method: 'get',
@@ -66,20 +63,20 @@ Page({
       header: {
         'content-type': 'application/x-www-form-urlencoded'
       },
-      success: function(res) {
-        var result = res.data.data;
+      success: function (res) {
+        let result = res.data.data;
         console.log(res.data.data);
-        for(var resultI = 0; resultI < result.length; resultI++) {
+        for (let resultI = 0; resultI < result.length; resultI++) {
           stuName.push(res.data.data[resultI].name);
           stuNum.push(res.data.data[resultI].stuNum);
         }
-        if(result.length >=7 ) {
+        if (result.length >= 7) {
           self.setData({
-            resultHight: "710rpx",
+            resultHight: '710rpx'
           });
         } else {
           self.setData({
-            resultHight: "",
+            resultHight: ''
           });
         }
         // console.log(stuName);
@@ -87,7 +84,7 @@ Page({
         self.setData({
           'searchResult.stuName': stuName,
           'searchResult.stuNum': stuNum,
-          resultHidden: false,
+          resultHidden: false
         });
       // console.log(self.data.index);
       }
@@ -97,17 +94,17 @@ Page({
   addstuNum: [],
   addstuMajor: [],
   addstuClass: [],
-  bindtapAdd: function bindtapAdd(e) {
-    var self = this;
+  bindtapAdd: function bindtapAdd (e) {
+    let self = this;
     wx.showToast({
       title: '数据获取中',
       icon: 'loading',
-      duration: 5000
+      duration: 10000
     });
     self.setData({
       nobodyHidden: true,
       somebodyHidden: false,
-      resultHidden: true,
+      resultHidden: true
     });
     console.log(e.currentTarget.dataset.stunum);
     wx.request({
@@ -119,59 +116,61 @@ Page({
       header: {
         'content-type': 'application/x-www-form-urlencoded'
       },
-      success: function(res) {
+      success: function (res) {
         console.log(res.data);
-        self.addstuName.push(res.data.data[0].name);
-        self.addstuNum.push(res.data.data[0].stuNum);
-        self.addstuMajor.push(res.data.data[0].college);
-        self.addstuClass.push(res.data.data[0].class);
-        self.setData({
-          'addStu.stuName': self.addstuName,
-          'addStu.stuNum': self.addstuNum,
-          'addStu.stuMajor': self.addstuMajor,
-          'addStu.stuClass': self.addstuClass,
-          anotherAdd: self.data.anotherAdd - 1,
-        });
-        console.log(self.addstuName.length);
-        if(self.addstuName.length == 6) {
+        if (self.addstuNum.indexOf(res.data.data[0].stuNum) === -1 && self.addstuName.length < 6) {
+          self.addstuName.push(res.data.data[0].name);
+          self.addstuNum.push(res.data.data[0].stuNum);
+          self.addstuMajor.push(res.data.data[0].college);
+          self.addstuClass.push(res.data.data[0].class);
           self.setData({
-            coverHidden: false,
+            'addStu.stuName': self.addstuName,
+            'addStu.stuNum': self.addstuNum,
+            'addStu.stuMajor': self.addstuMajor,
+            'addStu.stuClass': self.addstuClass,
+            anotherAdd: self.data.anotherAdd - 1
           });
-        } else {
-          self.setData({
-            coverHidden: true,
-          });
+          console.log(self.addstuName.length);
+          if (self.addstuName.length === 6) {
+            self.setData({
+              coverHidden: false
+            });
+          } else {
+            self.setData({
+              coverHidden: true
+            });
+          }
+          console.log(res.data.data[0].name);
         }
-        console.log(res.data.data[0].name);
       }
     });
   },
-  bindtapData: function bindtapData(e) {
-    var stuData = this.data.addStu;
+  bindtapData: function bindtapData (e) {
+    let stuData = this.data.addStu;
     console.log(this.data.addStu);
     wx.setStorage({
-      key:"key",
-      data: stuData,
+      key: 'key',
+      data: stuData
     });
-    if(this.addstuName.length == 0) {
+    if (this.addstuName.length === 0) {
       this.setData({
-        firstHidden: false,
+        firstHidden: false
       });
     } else {
       wx.navigateTo({
-        url: '../result/result',
+        url: '../result/result'
       });
     }
   },
-  bindtapClass: function bindtapClass(e) {
+  bindtapClass: function bindtapClass (e) {
     console.log(e.currentTarget.dataset);
     wx.navigateTo({
-        url: '../class/class?stuNumber=' + e.currentTarget.dataset.stunumber + '&stuName=' + e.currentTarget.dataset.stuname,
-      });
+      url: '../class/class?stuNumber=' + e.currentTarget.dataset.stunumber + '&stuName=' + e.currentTarget.dataset.stuname
+    });
   },
-  bindtapaddSure: function bindtapaddSure(e) {
+  bindtapaddSure: function bindtapaddSure (e) {
     this.setData({
-        firstHidden: true,
-      });
-  },
+      firstHidden: true
+    });
+  }
 });
