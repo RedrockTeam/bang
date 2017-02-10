2e74555285e9e81ad7aa861ff05fa33cb578e76c
lrb2333 <591392447@qq.com>
2017-02-10 23:53:46 +0800

    updata: 创建小杏韵页面

---
+++ src/pages/activity/xxy/xxy.js
@@ -0,0 +1,22 @@
+// const app = getApp();
+
+Page({
+  data: {
+    title: 'xxy'
+  },
+  onLoad () {
+    // TODO: onLoad
+  },
+  onReady () {
+    // TODO: onReady
+  },
+  onShow () {
+    // TODO: onShow
+  },
+  onHide () {
+    // TODO: onHide
+  },
+  onUnload () {
+    // TODO: onUnload
+  }
+});
