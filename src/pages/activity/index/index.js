5d6f34040c28eda65d69ab792b5f920e76d65054
lrb2333 <591392447@qq.com>
2017-02-26 18:51:20 +0800

    update: 活动首页的接口对完了~

--- src/pages/activity/index/index.js
+++ src/pages/activity/index/index.js
@@ -1,3 +1,4 @@
+const encodeFormated = require('../../../utils/util').encodeFormated;
 Page({
   data: {
     imgUrls: [
@@ -54,5 +55,69 @@ Page({
     this.setData({
       currentSwiper: e.detail.current
     });
+  },
+  onLoad () {
+    this.getData();
+  },
+  getData () {
+    const that = this;
+    const key = wx.getStorageSync('session');
+    const week = ['周日', '周一', '周二', '周三', '周四', '周五', '周六'];
+    var str = encodeFormated(wx.getStorageSync('session'));
+
+    wx.request({
+      method: 'post',
+      url: 'https://redrock.cqupt.edu.cn/weapp/Activity/Show/getHeadline',
+      data: {
+        params: str
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      success (res) {
+        var newImgUrls = [];
+        var type = 'school',
+            page = 1;
+        var str = encodeFormated(`${key}&${type}&${page}`);
+        for (var item of res.data.bags) {
+          newImgUrls.push(item.img);
+        }
+        that.setData({
+          imgUrls: newImgUrls
+        })
+        console.log(newImgUrls);
+        wx.request({
+          method: 'post',
+          url: 'https://redrock.cqupt.edu.cn/weapp/Activity/Show/getList',
+          data: {
+            params: str
+          },
+          header: {
+            'content-type': 'application/x-www-form-urlencoded'
+          },
+          success (res) {
+            var newActList = [];
+            for (var item of res.data.bags) {
+              var listItem = {};
+              var weekDay = week[new Date(item.date).getDay()];
+              var dateStr = item.date.split('-')[1];
+              if (dateStr < 10) {
+                dateStr = '0' + dateStr;
+              }
+              dateStr = dateStr + '/' +item.date.split('-')[2];
+              listItem.title = item.title;
+              listItem.date = dateStr;
+              listItem.day = weekDay;
+              listItem.place = item.place;
+              listItem.img = item.img;
+              newActList.push(listItem);
+            }
+            that.setData({
+              actList: newActList
+            });
+          }
+        })
+      }
+    })
   }
 });
